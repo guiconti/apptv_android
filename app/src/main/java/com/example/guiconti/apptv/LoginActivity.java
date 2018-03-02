3928280258540690543e93d5c493e2ca9864641a
Guilherme Conti <guibasconti@gmail.com>
2018-03-01 21:19:39 -0300

    Initial config commit

---
+++ app/src/main/java/com/example/guiconti/apptv/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.guiconti.apptv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
